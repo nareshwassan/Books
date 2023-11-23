2e3fa3b4c7ee5aaf4328aea1f6ac65e7db12df15
chary-vk <vchary43@gmail.com>
2023-11-23 22:04:13 +0530

    Add project files.

---
+++ Models/Books.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Books.Models
+{
+    [Table("books")]
+    public class Books
+    {
+        [Key]
+        public int BookId { get; set; }
+        public string BookName{ get; set; }
+    }
+}
